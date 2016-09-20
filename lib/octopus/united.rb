84826115c960755a345589eee6bc78d4a47263fb
vitaminus <partadw@gmail.com>
2016-09-20 16:51:14 +0300

    Remove puts errors backtrace

--- lib/octopus/united.rb
+++ lib/octopus/united.rb
@@ -286,7 +286,7 @@ module Octopus
         end
       rescue Exception => e
         puts e.message
-        puts e.backtrace.inspect
+        # puts e.backtrace.inspect
         if e.message.include?("failed to reach server, check DNS and/or server status")
           return 'united.com failed to reach server'
         end
