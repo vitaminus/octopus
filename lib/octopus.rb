c8276bde229e4d362401dbb0bd90be7e221337e9
vitaminus <partadw@gmail.com>
2016-09-28 18:56:38 +0300

    Add take method

--- lib/octopus.rb
+++ lib/octopus.rb
@@ -1,11 +1,17 @@
 require "octopus/version"
 require "octopus/united"
 require "octopus/aeroplan"
-require 'capybara'
-require 'capybara/poltergeist'
 
 module Octopus
-  def self.reopen
-    puts 'Octopus work fine!'
+
+  def self.take(klass_name, from, to, departure, params=nil)
+    return { errors: "Wrong date. Please enter date => #{Time.now.strftime('%F')}" } if DateTime.parse(departure).strftime('%F') < Time.now.strftime('%F')
+    begin
+      const_get(klass_name.capitalize).new(from, to, departure).get_data
+    rescue Exception => e
+      puts e.message
+      return { message: "#{klass_name.capitalize} currently switched off." }
+    end
   end
+
 end
